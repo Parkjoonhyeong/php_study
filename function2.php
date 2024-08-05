a1bbb99c1e8f3d62a88e4712c28d615c64b92e43
Joonhyeong Park <pjh010919@gmail.com>
2024-08-05 17:48:06 +0900

    Initial commit

---
+++ function2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>function</title>
+</head>
+<body>
+  <h1>Function</h1>
+  <h2>Basic</h2>
+  <?php
+    function basic(){
+      print("Lorem ipsum dolor1<br>");
+      print("Lorem ipsum dolor2<br>");
+    }
+    basic();
+    basic();
+    basic();
+    ?>
+  </body>
+</html>
---
+++ test.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<html>
+  <body>
+    <?php
+    echo date('Y-m-d:i:s');
+    ?>
+  </body>
+</html>
